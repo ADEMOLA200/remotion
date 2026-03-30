31e9860336e988e0079ff6965fb1b377b6f42c72
Pramod <pramodkumar.damam73@gmail.com>
2026-03-30 22:26:01 +0530

    Use crawlchat-client

--- packages/docs/src/theme/Layout/index.tsx
+++ packages/docs/src/theme/Layout/index.tsx
@@ -1,12 +1,16 @@
+import {useHistory} from '@docusaurus/router';
 import type {WrapperProps} from '@docusaurus/types';
 import '@remotion/promo-pages/dist/tailwind.css';
 import Layout from '@theme-original/Layout';
 import type LayoutType from '@theme/Layout';
+import {useCrawlChatSidePanel} from 'crawlchat-client';
 import React, {type ReactNode} from 'react';
 
 type Props = WrapperProps<typeof LayoutType>;
 
 const LayoutWrapper = (props: Props): ReactNode => {
+	useCrawlChatSidePanel({history: useHistory()});
+
 	return (
 		<>
 			<Layout {...props} />
@@ -16,12 +20,6 @@ const LayoutWrapper = (props: Props): ReactNode => {
 				id="crawlchat-script"
 				data-id="67c0a28c5b075f0bb35e5366"
 				data-sidepanel="true"
-				data-ask-ai="true"
-				data-ask-ai-background-color="#282A36"
-				data-ask-ai-color="#ffffff"
-				data-ask-ai-text="Ask AI"
-				data-ask-ai-position="br"
-				data-ask-ai-radius="20px"
 			/>
 		</>
 	);
